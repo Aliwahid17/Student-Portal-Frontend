3258465f84d0663e18131cdbd07e17b0b5d4be9a
wahid ali <88263575+Aliwahid17@users.noreply.github.com>
2022-11-09 17:55:37 +0530

    fix minor bug

--- pages/gcse/physics.tsx
+++ pages/gcse/physics.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/future/image'
 import React from 'react'
 import Options from '../../components/gcse/Options'
 import Physic from '../../public/gcse/physics.jpg'
@@ -7,9 +6,6 @@ const Physics = () => {
   return (
     
     <>
-    {/* <div className='flex '>
-       <Image src={Physic} alt='#' className='w-full h-auto' priority />
-    </div> */}
       <Options />
     </>
 
